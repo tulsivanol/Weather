46b573c43ca0b6d0a0bdba80696a3c20537f5233
tulsivanol <tulsivanol@gmail.com>
2021-03-19 09:26:14 +0530

    Initial Commit

---
+++ Weather MVVM/Weather_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_MVVMApp.swift
+//  Weather MVVM
+//
+//  Created by Tulsi Vanol on 19/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
